8eafc61c0bc33fae621081fff39ecf7a1a8bac1d
Alex Torres <alex910121@outlook.com>
2017-03-14 16:39:26 -0600

    PDO mail.php

--- php/mail.php
+++ php/mail.php
@@ -3,26 +3,22 @@
 $servername = "mysql.hostinger.mx";
 $username = "u464622211_alext";
 $password = "49_T3clas";
+$dbname = "	u464622211_dti";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
+    $sql = "INSERT INTO clients (name_client, phone, email, message, file, remembertoken)
+    VALUES ('$_POST["name_client"], $_POST["phone"], $_POST["email"], $_POST["message"], $_POST["file"], $_POST["remembertoken"])";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
     }
 catch(PDOException $e)
     {
-    echo "Connection failed: " . $e->getMessage();
+    echo $sql . "<br>" . $e->getMessage();
     }
-
-$sql = "INSERT INTO clients (name_client, phone, email, message, file, remembertoken)
-VALUES ($_POST["name_client"], $_POST["phone"], $_POST["email"], $_POST["message"], $_POST["file"], $_POST["remembertoken"]);"
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
 
 
 $conn = null;
