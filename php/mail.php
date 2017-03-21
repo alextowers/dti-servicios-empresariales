20bc713cb5f4dfaca8dc6fd918fe06d90ef2976c
Alex Torres <alex910121@outlook.com>
2017-03-20 19:49:14 -0600

    created php file that post the html form into db

--- php/mail.php
+++ php/mail.php
@@ -1,24 +1,26 @@
 <?php
 
-$servername = "mysql.hostinger.mx";
-$username = "u464622211_alext";
-$password = "49_T3clas";
-$dbname = "	u464622211_dti";
+ include 'connect.php';
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO clients (name_client, phone, email, message, file, remembertoken)
-    VALUES ('$_POST["name_client"]', '$_POST["phone"]', '$_POST["email"]', '$_POST["message"]', '$_POST["file"]', '$_POST["remembertoken"]')";
-    // use exec() because no results are returned
-    $conn->exec($sql);
+
+    $sql = $conn->prepare("INSERT INTO clients (name_client, phone, email, message, file, remembertoken) 
+    VALUES (:name_client, :phone, :email, :message, :file, :remembertoken)");
+
+    $sql->bindParam(':name_client', $_POST["name_client"]);
+    $sql->bindParam(':phone', $_POST["phone"]);
+    $sql->bindParam(':email', $_POST["email"]);
+    $sql->bindParam(':message', $_POST["message"]);
+    $sql->bindParam(':file', $_POST["file"]);
+    $sql->bindParam(':remembertoken', $_POST["remembertoken"]);
+    $sql->execute();
+    
     echo "New record created successfully";
     }
 catch(PDOException $e)
     {
     echo $sql . "<br>" . $e->getMessage();
     }
-
 
 $conn = null;
